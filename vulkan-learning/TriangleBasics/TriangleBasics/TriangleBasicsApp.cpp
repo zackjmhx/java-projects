5a758106b7cc867b05db4abf3edf3a9d76d0dcda
Zachary Margulies Hendrix <32447406+zackjmhx@users.noreply.github.com>
2018-01-25 14:18:34 -0500

    Add texture binding in createDescriptorSetLayout

--- vulkan-learning/TriangleBasics/TriangleBasics/TriangleBasicsApp.cpp
+++ vulkan-learning/TriangleBasics/TriangleBasics/TriangleBasicsApp.cpp
@@ -780,6 +780,7 @@ private:
 	}
 
 	void createDescriptorSetLayout() {
+
 		VkDescriptorSetLayoutBinding uboLB = {};
 		uboLB.binding = 0;
 		uboLB.descriptorCount = 1;
@@ -787,10 +788,19 @@ private:
 		uboLB.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
 		uboLB.pImmutableSamplers = nullptr;
 
+		VkDescriptorSetLayoutBinding samplerLB = {};
+		samplerLB.binding = 1;
+		samplerLB.descriptorCount = 1;
+		samplerLB.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+		samplerLB.stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT;
+		samplerLB.pImmutableSamplers = nullptr;
+
+		std::array<VkDescriptorSetLayoutBinding, 2> bindings = { uboLB, samplerLB };
+
 		VkDescriptorSetLayoutCreateInfo layoutInfo = {};
 		layoutInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
-		layoutInfo.bindingCount = 1;
-		layoutInfo.pBindings = &uboLB;
+		layoutInfo.bindingCount = static_cast<uint32_t>(bindings.size());
+		layoutInfo.pBindings = bindings.data();
 
 		if (vkCreateDescriptorSetLayout(device, &layoutInfo, nullptr, &desSetLayout) != VK_SUCCESS)
 			throw std::runtime_error("Failed to create descriptor set layouts.");
