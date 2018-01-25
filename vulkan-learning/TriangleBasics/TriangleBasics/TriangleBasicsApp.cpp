d95f3884581da5935ad7d1d9607b03ced84d7d4b
Zachary Margulies Hendrix <32447406+zackjmhx@users.noreply.github.com>
2018-01-25 14:07:19 -0500

    Turn on anisotropy feature on device

--- vulkan-learning/TriangleBasics/TriangleBasics/TriangleBasicsApp.cpp
+++ vulkan-learning/TriangleBasics/TriangleBasics/TriangleBasicsApp.cpp
@@ -512,6 +512,7 @@ private:
 
 
 		VkPhysicalDeviceFeatures deviceFeatures = {};
+		deviceFeatures.samplerAnisotropy = VK_TRUE;
 
 		VkDeviceCreateInfo createInfo = {};
 		createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
