7c72919d461227fc7b443b02f1ab15dfebcd1da6
zackjmhx <zack.jmh@gmail.com>
2018-01-27 22:56:26 -0500

    Add createDepthResources

--- vulkan-learning/TriangleBasics/TriangleBasics/TriangleBasicsApp.cpp
+++ vulkan-learning/TriangleBasics/TriangleBasics/TriangleBasicsApp.cpp
@@ -1027,7 +1027,23 @@ private:
 	}
 
 	void createDepthResources() {
+	bool hasStencilComponent(VkFormat format) {
+		return format == VK_FORMAT_D32_SFLOAT_S8_UINT || format == VK_FORMAT_D24_UNORM_S8_UINT;
+	}
 
+	VkFormat findSupportedFormat(const std::vector<VkFormat> &candidates, VkImageTiling tiling, VkFormatFeatureFlags features) {
+		for (VkFormat format : candidates) {
+			VkFormatProperties props;
+			vkGetPhysicalDeviceFormatProperties(physicalDevice, format, &props);
+
+			if (tiling == VK_IMAGE_TILING_LINEAR && (props.linearTilingFeatures & features) == features){
+				return format;
+			} else if (tiling == VK_IMAGE_TILING_OPTIMAL && (props.optimalTilingFeatures & features) == features) {
+				return format;
+			}
+
+			throw std::runtime_error("Failed to find supported format!");
+		}
 	}
 
 	void createTextureImage() {
