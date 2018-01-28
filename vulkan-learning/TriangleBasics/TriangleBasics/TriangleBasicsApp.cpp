3a4115a39753c621941d619af2adaee4b93fc364
zackjmhx <zack.jmh@gmail.com>
2018-01-28 16:05:40 -0500

    Add loadModel definition

--- vulkan-learning/TriangleBasics/TriangleBasics/TriangleBasicsApp.cpp
+++ vulkan-learning/TriangleBasics/TriangleBasics/TriangleBasicsApp.cpp
@@ -1189,6 +1189,49 @@ private:
 	}
 
 
+	void loadModel(const std::string fileName) {
+		tinyobj::attrib_t attrib;
+		std::vector<tinyobj::shape_t> shapes;
+		std::vector<tinyobj::material_t> materials;
+		std::string err;
+
+		if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &err, (MODEL_PATH_ROOT + fileName).c_str()))
+			throw std::runtime_error(err);
+
+		std::unordered_map<Vertex, uint32_t> uniqueVerticies = {};
+
+		for (const auto &shape : shapes) {
+			for (const auto &index : shape.mesh.indices) {
+				Vertex vertex = {};
+
+				vertex.pos = {
+					attrib.vertices[3 * index.vertex_index + 0],
+					attrib.vertices[3 * index.vertex_index + 1],
+					attrib.vertices[3 * index.vertex_index + 2]
+				};
+
+				vertex.tex = {
+					attrib.texcoords[2 * index.texcoord_index + 0],
+					1.0f - attrib.texcoords[2 * index.texcoord_index + 1] //Fix obj - vulkan coord system mismatch
+				};
+
+				vertex.color = { 1.0f, 1.0f, 1.0f };
+
+				if (uniqueVerticies.count(vertex) == 0) {
+					uniqueVerticies[vertex] = static_cast<uint32_t>(vertices.size());
+					vertices.push_back(vertex);
+				}
+
+				vIndices.push_back(uniqueVerticies[vertex]);
+			}
+		}
+
+#ifndef NDEBUG
+		std::cout << "Finished loading model." << std::endl;
+#endif
+	}
+
+
 	void createVertexBuffer() {
 		VkDeviceSize bufferSize = sizeof(vertices[0]) * vertices.size();
 
