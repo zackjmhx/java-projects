f4c32d1e451720dec4082eee59377602a55c03d3
zackjmhx <zack.jmh@gmail.com>
2018-01-28 16:06:24 -0500

    Remove hardcoded vertex and index information, add member vectors for both

--- vulkan-learning/TriangleBasics/TriangleBasics/TriangleBasicsApp.cpp
+++ vulkan-learning/TriangleBasics/TriangleBasics/TriangleBasicsApp.cpp
@@ -197,6 +197,9 @@ private:
 
 	std::vector<VkFramebuffer> swapChainFramebuffers;
 
+	std::vector<Vertex> vertices;
+	std::vector<uint32_t> vIndices;
+
 	VkBuffer vertexBuffer;
 	VkDeviceMemory vertexBufferMemory;
 
@@ -226,23 +229,6 @@ private:
 	VkSemaphore renderFinishedSemaphore;
 
 	QueueFamilyIndices indicies;
-
-	const std::vector<Vertex> vertices = {
-		{ { -0.5f, -0.5f, 0.0f },{ 1.0f, 0.0f, 0.0f },{ 1.0f, 0.0f } },
-		{ { 0.5f, -0.5f, 0.0f },{ 0.0f, 1.0f, 0.0f },{ 0.0f, 0.0f } },
-		{ { 0.5f, 0.5f, 0.0f },{ 0.0f, 0.0f, 1.0f },{ 0.0f, 1.0f } },
-		{ { -0.5f, 0.5f, 0.0f },{ 1.0f, 1.0f, 1.0f },{ 1.0f, 1.0f } },
-		
-		{ { -0.5f, -0.5f, -0.5f },{ 1.0f, 0.0f, 0.0f },{ 1.0f, 0.0f } },
-		{ { 0.5f, -0.5f, -0.5f },{ 0.0f, 1.0f, 0.0f },{ 0.0f, 0.0f } },
-		{ { 0.5f, 0.5f, -0.5f },{ 0.0f, 0.0f, 1.0f },{ 0.0f, 1.0f } },
-		{ { -0.5f, 0.5f, -0.5f },{ 1.0f, 1.0f, 1.0f },{ 1.0f, 1.0f } }
-	};
-
-	const std::vector<uint16_t> vIndicies = {
-		0, 1, 2, 2, 3, 0,
-		4, 5, 6, 6, 7, 4
-	};
 
 
 	void initWindow() {
