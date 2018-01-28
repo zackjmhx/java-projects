dddf9e765a65a5170b94eb123e83dbd401ba7614
zackjmhx <zack.jmh@gmail.com>
2018-01-28 01:40:05 -0500

    Add depth ClearValue data to the render pass

--- vulkan-learning/TriangleBasics/TriangleBasics/TriangleBasicsApp.cpp
+++ vulkan-learning/TriangleBasics/TriangleBasics/TriangleBasicsApp.cpp
@@ -1402,9 +1402,12 @@ private:
 			renderPassInfo.renderArea.extent = swapChainExtent;
 			renderPassInfo.renderArea.offset = { 0, 0 };
 
-			VkClearValue clearColor = { 0.0f, 0.0f, 0.0f, 1.0f };
-			renderPassInfo.clearValueCount = 1;
-			renderPassInfo.pClearValues = &clearColor;
+			std::array<VkClearValue, 2> clearValues = {};
+			clearValues[0].color = { 0.0f, 0.0f, 0.0f, 1.0f };
+			clearValues[1].depthStencil = { 1.0f, 0 };
+
+			renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
+			renderPassInfo.pClearValues = clearValues.data();
 
 			vkCmdBeginRenderPass(commandBuffers[i], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
 
